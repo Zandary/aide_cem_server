7ef49d3e11e25f563cb4fda7c5a9746a9c7b37f5
unknown <haritiana32.48@gmail.com>
2023-08-10 18:15:47 +0300

    Mety le connexion am db fa le route sy request amzay zao

---
+++ server.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB Connected…')
+  })
+  .catch(err => console.log(err))
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
