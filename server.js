555c73437ab1c005d0ddc74857cb4bd84a35b559
unknown <haritiana32.48@gmail.com>
2023-08-15 10:38:08 +0300

    novaina le port

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   })
   .catch(err => console.log(err))
 
-app.listen(3000, () => {
+app.listen(33455, () => {
   console.log('Server is running on port 3000');
 });
